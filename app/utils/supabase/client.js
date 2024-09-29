fe0ad12f9422ef0a92434a78c06111b98989d4c9
Batuhan Çoban <batuhancoban@Batuhan-MacBook-Pro.local>
2024-09-29 18:54:29 +0300

    build sorunu devam ediyor

--- app/utils/supabase/client.js
+++ app/utils/supabase/client.js
@@ -19,3 +19,6 @@ if (!supabaseUrl || !supabaseAnonKey) {
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log('Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log('Supabase Anon Key:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
